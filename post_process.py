b61272e4aea38c16da1d7fe3ca95b56c8701565f
Joseph Boyd <joseph.boyd@epfl.ch>
2015-03-06 16:32:04 +0100

    added metadata encoding information

--- post_process.py
+++ post_process.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import sys
 from bs4 import BeautifulSoup
 import json
@@ -50,6 +49,28 @@ def convert_to_json(path):
     back = doc.tei.back
 
     # Process header
+
+    """
+    ENCODING OF HEADERS - grobid/grobid-trainer/doc/GuidelinesTrainingData.pdf
+    <titlePart> for title (included in a <docTitle> element).
+    <docAuthor> for the complete sequence of authors.
+    <affiliation> for the complete affiliation field.
+    <address> for the complete address field. 
+    <div type="introduction"> for the start of the introduction section (i.e. just after the header ends).
+    <keyword> for the keyword field
+    <email> for encoding the email address
+    <phone> for encoding the contact phone number
+    <ptr> for web url corresponding to the processed document (e.g. where the document is available online).
+    <date type="publication"> is the publication date, which is similar to the default case
+    <reference> this is to annotate the reference information about the current article present in its header.
+    <idno> for the article-specific identifier, in particular DOI.
+    <note type="copyright"> for the copyright info
+    <note type="submission"> for the information about the submission of the document.
+    <note type="dedication"> for information about the dedication of the publication.
+    <note type="page"> for the page number
+    <note type="english_title"> in the case that the main title is not in English.
+    """
+
     header_data = {}
     
     header_data['idno'] = header.idno.text if header.idno is not None else None
@@ -58,9 +79,29 @@ def convert_to_json(path):
     header_data['title'] = header.title.text if header.title is not None else None
     header_data['abstract'] = front.p.text if header.p is not None else None
     header_data['authors'] = [{"forename" : a.forename.text, "surname" : a.surname.text, "orgName" : a.orgname.text} \
-                              for a in header.sourcedesc.biblstruct.analytic.findAll("author")]       
+                              for a in header.sourcedesc.biblstruct.analytic.findAll("author")]
 
     # Process references
+
+    """
+    ENCODING OF REFERENCES - grobid/grobid-trainer/doc/GuidelinesTrainingData.pdf
+    <author> for the complete sequence of authors
+    <title level="a"> for article title and chapter title.
+    <title level="j"> for journal title.
+    <title level="m"> for non journal bibliographical item holding the cited article.
+    <date> the date sequence (including parenthesis, etc.)
+    <biblScope type="pp"> the full range of pages
+    <biblScope type="vol"> the block for volume (e.g. <volume> vol. 7,</volume>)
+    <biblScope type="issue"> the block for the issue, also known as number.
+    <orgName> the institution for thesis or technical reports
+    <publisher> the name of the publisher
+    <pubPlace> publication place, or location of the "publishing" institution
+    <editor> for all the sequence of editors
+    <ptr> for web url
+    <idno> for the document-specific identifier, in particular DOI
+    <note> for any indications related to the reference and not covered by one of the previous tags.
+    """
+
     reference_data = []
     i = 0
 
