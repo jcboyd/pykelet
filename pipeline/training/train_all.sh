6c3ac04867f348b9e8b6c51d0eb829af80b1cc22
jcboyd <boyd.joseph.c@gmail.com>
2015-04-14 21:07:37 +0200

    fixed indentation

--- pipeline/training/train_all.sh
+++ pipeline/training/train_all.sh
@@ -3,12 +3,12 @@
 if test "$#" -ne 2; then
     echo "Illegal number of parameters. Usage: bash trial.sh [PATH-TO-GROBID] [PATH-TO-LOG-FOLDER]"
 else
-	declare -a models=("affiliation" "chemical" "date" "citation" "ebook" \
-				"fulltext" "header" "name-citation" "name-header" \
-				"patent" "segmentation" "reference-segmenter")
+    declare -a models=("affiliation" "chemical" "date" "citation" "ebook" \
+                "fulltext" "header" "name-citation" "name-header" \
+                "patent" "segmentation" "reference-segmenter")
 
-	for i in "${models[@]}"
-	do
-		java -Xmx1024m -jar $1"grobid/grobid-trainer/target/grobid-trainer.jar" "2" "$i" "-gH" "grobid/grobid-home" "-s" "0.8" > $2/"$i".log
-	done
+    for i in "${models[@]}"
+    do
+        java -Xmx1024m -jar $1"grobid/grobid-trainer/target/grobid-trainer.jar" "2" "$i" "-gH" "grobid/grobid-home" "-s" "0.8" > $2/"$i".log
+    done
 fi
