77bcd2abf59cd253adb8d24348eaca9423098300
Joseph Boyd <joseph.boyd@epfl.ch>
2015-04-28 11:23:43 +0200

    fixed something

--- pipeline/processing/k_fold_cross_validation.py
+++ pipeline/processing/k_fold_cross_validation.py
@@ -9,9 +9,9 @@ from grobid import GrobidTrainer
 def k_fold_cross_validation(grobid, model, n_folds, log_path):
     grobid_home = grobid + '/grobid-home'
     corpus = grobid + \
-        '/grobid-trainer/resources/dataset/%s/corpus/tei/' % (model)
+        '/grobid-trainer/resources/dataset/%s/corpus//' % (model)
     evaluation = grobid + \
-        '/grobid-trainer/resources/dataset/%s/evaluation/tei/' % (model)
+        '/grobid-trainer/resources/dataset/%s/evaluation//' % (model)
     classpath_trainer = grobid + \
         '/grobid-trainer/target/grobid-trainer-0.3.4-SNAPSHOT.jar'
 
@@ -21,6 +21,8 @@ def k_fold_cross_validation(grobid, model, n_folds, log_path):
 
     folds = list(KFold(len(training_set), n_folds=n_folds))
 
+    i = 1
+
     for fold in folds:
         try:
             # move all fold files to evaluate folder
@@ -28,8 +30,8 @@ def k_fold_cross_validation(grobid, model, n_folds, log_path):
                 shutil.move(corpus + training_set[index], evaluation)
 
             grobid_trainer.train(model)
-            grobid_trainer.evaluate(model, log_path + "/%s_%s" %
-                                   (model, folds.index(fold)))
+            grobid_trainer.evaluate(model, log_path + "/%s_%s" % (model, i))
+            i += 1
         except IOError:
             print 'Error: check folder configuration'
         finally:
